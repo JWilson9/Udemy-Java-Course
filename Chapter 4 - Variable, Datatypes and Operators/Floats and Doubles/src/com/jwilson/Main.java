d5c0f74018a7cc68e501a14a788987f6772a3e54
james.wilson2 <james.wilson2@mydit.ie>
2016-12-13 00:10:31 +0000

    Float study

--- Chapter 4 - Variable, Datatypes and Operators/Floats and Doubles/src/com/jwilson/Main.java
+++ Chapter 4 - Variable, Datatypes and Operators/Floats and Doubles/src/com/jwilson/Main.java
@@ -4,5 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int myValue = 5;
+        float myFloatValue = 5f;
+        double myDoubleValue = 5d;
+
+        System.out.println("int value" + myValue);
+        System.out.println("float value" + myFloatValue);
+        System.out.println("double value" + myDoubleValue);
+
+        int pounds = 7;
+        double myDouble = (pounds * 0.45359237d);
+
+        System.out.println(myDouble);
+        //0.45359237d
+
+
     }
 }
