a8b52da8066c788e35f9ba82edd8efb7e4bc6769
james.wilson2 <james.wilson2@mydit.ie>
2016-12-18 01:07:40 +0000

    Changes to method main file
    Custom changes to method main file

--- Chapter 5 - Expressions, Statements, methods and more/Methods/src/com/jwilson/Main.java
+++ Chapter 5 - Expressions, Statements, methods and more/Methods/src/com/jwilson/Main.java
@@ -1,5 +1,7 @@
 package com.jwilson;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,22 +10,65 @@ public class Main {
         int levelCompleted = 5;
         int bonus = 100;
 
-        calculateScore(gameOver, score, levelCompleted, bonus);
+        int firstScore = calculateScore(gameOver, score, levelCompleted, bonus);
 
-        score = 800;
-        levelCompleted = 5;
-        bonus = 100;
+        score = 1000;
+        levelCompleted = 7;
+        bonus = 200;
 
-        calculateScore(gameOver, score, levelCompleted, bonus);
+        int secondScore = calculateScore(gameOver, score, levelCompleted, bonus);
+
+        System.out.println(" first score is: " + firstScore + " and second score is: " + secondScore);
+
+        System.out.println("please enter your name below");
+        Scanner scan = new Scanner(System.in);
+        String playerName = scan.nextLine();
+
+        System.out.println("please enter your score below");
+        Scanner scanScore = new Scanner(System.in);
+        scanScore = new Scanner(System.in);
+        int highScore = scanScore.nextInt();
+
+        //displayHighScorePosition(playerName, highScore);
+
+        int playerCorrectScore = calculateHighScore(highScore);
+
+        String player1 = displayHighScorePosition(playerName, playerCorrectScore);
+        System.out.println(player1);
 
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
 
         if(gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
             System.out.println("Your final score was " + finalScore);
+            return(finalScore);
+        }else{
+            return -1;
         }
     }
+
+    public static String displayHighScorePosition(String playerName, int highScore){
+        return playerName + " " + Integer.toString(highScore);
+    }
+
+
+
+    public static int calculateHighScore(int highScore){
+        if (highScore > 1000 ){
+            return highScore;
+        }else if(highScore > 500 && highScore < 1000){
+            return highScore;
+        }
+        else if(highScore > 100 || highScore < 500){
+            return highScore;
+        }else{
+            highScore = 0;
+            return highScore;
+        }
+
+    }
+
 }
