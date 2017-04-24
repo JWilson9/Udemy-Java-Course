d3c3f9a502b80372b7af66eef2599b11bf1a8b89
James Wilson <james@morsolutions.com>
2017-04-24 18:10:29 +0100

    commiting composition

---
+++ Composition/src/Resolution.java
@@ -0,0 +1,23 @@
+/**
+ * Created by james on 24/04/2017.
+ */
+public class Resolution {
+
+    private int width;
+    private int height;
+
+
+    public Resolution(int width, int height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+}
